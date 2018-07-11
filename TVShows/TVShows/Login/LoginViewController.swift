f4f78c695cdf04dc2dfbc9098b2648b752651f0e
IvanMilicevic <ivan.milicevic.zu@gmail.com>
2018-07-11 15:19:33 +0200

    add some bling (constraints, font, background, rounded edges, activity indicator...)

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -11,11 +11,38 @@ import UIKit
 class LoginViewController: UIViewController {
 
     var tapCounter:Int = 0
+    let labelText:String = "Dabs: "
+    var activityIndicator:UIActivityIndicatorView = UIActivityIndicatorView()
     
     @IBOutlet weak var loginLabel: UILabel!
     
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imageView.isHidden=true
+        loginButton.isEnabled=false
+        
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.white
+        view.addSubview(activityIndicator)
+        
+        activityIndicator.startAnimating()
+        
+        DispatchQueue.main.async {
+           sleep(3)
+            self.activityIndicator.stopAnimating()
+            
+            self.imageView.isHidden=false
+            self.loginButton.isEnabled=true
+        }
+        
+        
 
         // Do any additional setup after loading the view.
     }
@@ -28,7 +55,15 @@ class LoginViewController: UIViewController {
 
     @IBAction func buttonPressed(_ sender: Any) {
         tapCounter+=1
-        loginLabel.text = "Taps: " + String(tapCounter)
+        loginLabel.text = labelText + String(tapCounter)
+        
+        if(tapCounter % 2 == 0){
+            imageView.image = UIImage(named: "Dab1")
+        }else{
+            imageView.image = UIImage(named: "Dab2")
+        }
+        
+        
     }
     
 
