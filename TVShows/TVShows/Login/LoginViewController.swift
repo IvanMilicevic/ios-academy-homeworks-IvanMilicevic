e75be62086a498c5bacd0a803af2c26896d5cea7
IvanMilicevic <ivan.milicevic.zu@gmail.com>
2018-07-19 13:07:11 +0200

    add Log in and Create an account functionality, (also added User and LoginData)

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import SVProgressHUD
+import Alamofire
+import CodableAlamofire
 
 class LoginViewController: UIViewController {
 
@@ -19,15 +22,19 @@ class LoginViewController: UIViewController {
     // MARK: - Private
     private var rememberState: Bool = false
     private var loginCornerRadius: CGFloat = 10
+    private var user: User?
+    private var loginData: LoginData?
     
     // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        SVProgressHUD.setDefaultMaskType(.black)
         
         loginButton.layer.cornerRadius = loginCornerRadius
-        usernameTextField.setBottomBorder()
-        passwordTextField.setBottomBorder()
+        usernameTextField.setBottomBorderDefault()
+        passwordTextField.setBottomBorderDefault()
     }
+    
     
     // MARK: - IBActions
     @IBAction func rememberMeButtonPressed(_ sender: Any) {
@@ -41,23 +48,108 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func loginButtonPressed(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "Home", bundle: nil)
-        let viewControllerD = storyboard.instantiateViewController(withIdentifier: "ViewController_Home")
+        let email : String = usernameTextField.text!
+        let password : String = passwordTextField.text!
         
-        navigationController?.pushViewController(viewControllerD, animated: true)
+        if !isLoginOk(email: email, password: password){
+            return
+        }
+        
+        SVProgressHUD.show()
+        loginUserWith(email: email, password: password)
     }
     
     @IBAction func createAnAccountButtonPressed(_ sender: Any) {
+        let email : String = usernameTextField.text!
+        let password : String = passwordTextField.text!
+        
+        if !isLoginOk(email: email, password: password){
+            return
+        }
+        
+        SVProgressHUD.show()
+        
+        let parameters: [String: String] = [
+            "email": email,
+            "password": password
+        ]
+        
+        Alamofire
+            .request("https://api.infinum.academy/api/users",
+                     method: .post,
+                     parameters: parameters,
+                     encoding: JSONEncoding.default)
+            .validate()
+            .responseDecodableObject(keyPath: "data", decoder: JSONDecoder()) {
+                (response: DataResponse<User>) in
+                switch response.result {
+                    case .success(let user):
+                        self.user=user
+                        print("Success: \(user)")
+                        self.loginUserWith(email: email, password: password)
+                    case .failure(let error):
+                        SVProgressHUD.showError(withStatus: "Error")
+                        print("API failure: \(error)")
+                }
+            }
+    }
+    
+    
+    // MARK: - Private Functions
+    private func isLoginOk(email: String, password : String) -> Bool{
+        var loginIsOk = true;
+        if(email.isEmpty){
+            usernameTextField.setBottomBorderRed()
+            loginIsOk=false
+        }else{
+            usernameTextField.setBottomBorderDefault()
+        }
+        if(password.isEmpty){
+            passwordTextField.setBottomBorderRed()
+            loginIsOk=false
+        }else{
+            passwordTextField.setBottomBorderDefault()
+        }
+        
+        return loginIsOk
+    }
+    
+    private func loginUserWith(email: String, password : String) {
         let storyboard = UIStoryboard(name: "Home", bundle: nil)
         let viewControllerD = storyboard.instantiateViewController(withIdentifier: "ViewController_Home")
         
-        navigationController?.pushViewController(viewControllerD, animated: true)
+        let parameters: [String: String] = [
+            "email": email,
+            "password": password
+        ]
+        
+        Alamofire
+            .request("https://api.infinum.academy/api/users/sessions",
+                     method: .post,
+                     parameters: parameters,
+                     encoding: JSONEncoding.default)
+            .validate()
+            .responseDecodableObject(keyPath: "data", decoder: JSONDecoder()) {
+                (response: DataResponse<LoginData>) in
+                switch response.result {
+                case .success(let loginData):
+                    self.loginData=loginData
+                    print("Success: \(loginData)")
+                    SVProgressHUD.showSuccess(withStatus: "Success")
+                    self.navigationController?.pushViewController(viewControllerD, animated: true)
+                case .failure(let error):
+                    SVProgressHUD.showError(withStatus: "Error")
+                    print("Failure: \(error)")
+                }
+            }
     }
+    
+    
     
 }
 
 extension UITextField {
-    func setBottomBorder() {
+    func setBottomBorderDefault() {
         self.borderStyle = .none
         self.layer.backgroundColor = UIColor.white.cgColor
         
@@ -67,4 +159,16 @@ extension UITextField {
         self.layer.shadowOpacity = 1.0
         self.layer.shadowRadius = 0.0
     }
+    
+    func setBottomBorderRed() {
+        self.borderStyle = .none
+        self.layer.backgroundColor = UIColor.white.cgColor
+        
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = UIColor.red.cgColor
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+        self.layer.shadowOpacity = 1.0
+        self.layer.shadowRadius = 0.0
+    }
+    
 }
