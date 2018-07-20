2f49bff3cc0c44c6dddcdae2ea4d02c9d0553d31
IvanMilicevic <ivan.milicevic.zu@gmail.com>
2018-07-20 12:30:00 +0200

    Update indentation.

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -34,8 +34,15 @@ class LoginViewController: UIViewController {
         
         loginButton.layer.cornerRadius = loginCornerRadius
         
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name:NSNotification.Name.UIKeyboardWillShow, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name:NSNotification.Name.UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow),
+                                               name:NSNotification.Name.UIKeyboardWillShow,
+                                               object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name:NSNotification.Name.UIKeyboardWillHide,
+                                               object: nil)
         usernameTextField.setBottomBorderDefault()
         passwordTextField.setBottomBorderDefault()
     }
