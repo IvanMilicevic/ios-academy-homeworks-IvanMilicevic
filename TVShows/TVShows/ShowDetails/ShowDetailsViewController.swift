c6ef169a6490a3c9864508c74c038a7ab4045843
IvanMilicevic <ivan.milicevic.zu@gmail.com>
2018-07-24 11:38:51 +0200

    implement api call for Show Details

--- TVShows/TVShows/ShowDetails/ShowDetailsViewController.swift
+++ TVShows/TVShows/ShowDetails/ShowDetailsViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import SVProgressHUD
+import Alamofire
+import CodableAlamofire
 
 class ShowDetailsViewController: UIViewController {
     
@@ -25,11 +28,63 @@ class ShowDetailsViewController: UIViewController {
     var loginData: LoginData?
     var showID: String?
     
+    // MARK: - Private
+    private var showDetails: ShowDetails?
+    
     // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        fetchShowDetails()
         // Do any additional setup after loading the view.
+    }
+    
+    
+    // MARK: - Private Functions
+    private func fetchShowDetails() {
+        guard
+            let token=loginData?.token
+            else {
+                return
+        }
+        let headers = ["Authorization": token]
+        
+        SVProgressHUD.show()
+        
+        Alamofire
+            .request("https://api.infinum.academy/api/shows/\(showID!)",
+                     method: .get,
+                     encoding: JSONEncoding.default,
+                     headers: headers)
+            .validate()
+            .responseDecodableObject(keyPath: "data", decoder: JSONDecoder()) {
+                (response: DataResponse<ShowDetails>) in
+                switch response.result {
+                case .success(let details):
+                    SVProgressHUD.dismiss()
+                    print("Show Details fetched: \(details)")
+                    self.showDetails=details
+//                    self.homeTableView.reloadData()
+                case .failure(let error):
+                    SVProgressHUD.dismiss()
+                    print("Fetching show details went wrong: \(error)")
+                    let alertController = UIAlertController(title: "Error",
+                                                            message: error.localizedDescription,
+                                                            preferredStyle: .alert)
+                    alertController.addAction(UIAlertAction(title: "OK", style: .cancel){
+                        (action:UIAlertAction) in
+                        self.returnToHomeScreen()
+                    })
+                    self.present(alertController, animated: true, completion: nil)
+                    
+                }
+        }
+    }
+    
+    private func returnToHomeScreen(){
+        let storyboard = UIStoryboard(name: "Login", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "ViewController_Login")
+        self.navigationController?.pushViewController(viewController, animated: true)
     }
 
 }
