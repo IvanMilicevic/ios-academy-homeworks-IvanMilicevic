c223804f3748ca634a0dc224d56ed906638e33e0
IvanMilicevic <ivan.milicevic.zu@gmail.com>
2018-07-24 11:01:22 +0200

    create ShowDetails codeable model

--- TVShows/TVShows/Common/CodableStorage.swift
+++ TVShows/TVShows/Common/CodableStorage.swift
@@ -17,6 +17,26 @@ struct Show: Codable {
         case id = "_id"
         case title
         case likesCount
+    }
+}
+
+struct ShowDetails: Codable {
+    let type: String
+    let title: String
+    let description: String
+    let id: String
+    let likesCount: Int?
+    let imageUrl: String
+    
+    enum CodingKeys: String, CodingKey {
+        case type
+        case title
+        case description
+        case id = "_id"
+        case likesCount
+        case imageUrl
         
     }
+    
+    
 }
